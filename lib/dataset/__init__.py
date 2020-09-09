0dc9bb866a6cea9743f533ade7717d6df0854bf6
Kulbear <jiyang.py@gmail.com>
2020-09-08 23:52:59 -0600

    Duplicate a meta-learning dataset

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -8,4 +8,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .mpii import MPIIDataset as mpii
+from .mpii import MPIIDataset, MetaMPIIDataset
+
